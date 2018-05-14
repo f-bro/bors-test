955fb48e02e40b87315408d42bcb0fffbfa24f4f
Fabio B <4430543+f-bro@users.noreply.github.com>
2018-05-14 20:53:34 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     println!("Hello, world!");
     println!("Hello, Bors!");
     println!("Hello, Bors2!");
-           println!("Hello, Bors2!");
+    println!("Hello, Bors2!");
     println!("Hello, Bors2!");
     println!("Miau");
 }
